2763d479a962473aa4815fe1141674bf34ba8dd3
Hector Perez <hperez1993@yahoo.com>
2019-02-04 04:45:39 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(abc)
+  return "Hello, my name is #{abc}."
+end
+
+def batch_badge_creator(attendees)
+  
+ attendees.collect do |name|
+   "Hello, my name is #{name}."
+end
+end
+
+def assign_rooms(attender)
+  counter = 0
+  attendees.collect do |attendee|
+  counter += 1
+  "Hello, #{attendee}! You'll be assigned to room #{counter}!"
+end
+end
+
+def printer
+  
+end
